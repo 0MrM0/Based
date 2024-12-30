e337d8b835ee8f2372547a8302db453803f340bf
0MrM0 <144493022+0MrM0@users.noreply.github.com>
2024-12-30 09:41:59 +0330

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x5657dec7626fd93a54015eca4f1e3f6c115bd7fa";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
